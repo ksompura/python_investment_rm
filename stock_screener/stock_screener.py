e7c0c8247359e5bbb5aecd1bd6240f3843d9273e
Keshav Sompura <keshavsomp@gmail.com>
2021-03-22 09:24:53 -0700

    Add option to use volume as a filter

--- stock_screener/stock_screener.py
+++ stock_screener/stock_screener.py
@@ -15,14 +15,14 @@ root = Tk()
 ftypes = [(".xlsm","*.xlsx",".xls")]
 ttl  = "Title"
 dir1 = 'C:\\'
-filePath = askopenfilename(filetypes = ftypes, initialdir = dir1, title = ttl)
-# filePath=r"C:\Users\Keshav Sompura\Documents\Python Projects\RichardStocks.xlsx"
+# filePath = askopenfilename(filetypes = ftypes, initialdir = dir1, title = ttl)
+filePath=r"C:\Users\Keshav Sompura\Documents\Python Projects\RichardStocks.xlsx"
 
 stocklist = pd.read_excel(filePath)
-stocklist=stocklist[200:400] # WARNING remove if want to screen all stocks in list, want to check if you are allowed to scrape thousands of stock 
+stocklist=stocklist[:200] # WARNING remove if want to screen all stocks in list, want to check if you are allowed to scrape thousands of stock 
 # print(stocklist)
 
-export_list = pd.DataFrame(columns =["Stock", "50 Day MA", "150 Day MA", "200 Day MA", "52 Week Low", "52 Week High"])
+export_list = pd.DataFrame(columns =["Stock", "50 Day MA", "150 Day MA", "200 Day MA", "52 Week Low", "52 Week High"]) #"52 Week Vol High"
 
 for i in stocklist.index:
     stock = str(stocklist["Symbol"][i])
@@ -34,7 +34,12 @@ for i in stocklist.index:
         sma_used = [50,150,200]
         for x in sma_used:
             sma = x
+            #Find simple moving average for volume and 
+            # df[f"Vol_SMA_{sma}"] = df.iloc[:,6].rolling(window=sma).mean()
             df[f"SMA_{sma}"] = round(df.iloc[:,4].rolling(window=sma).mean(),2)
+            
+        # print(df)    
+        # break
         
         current_close = df["Adj Close"][-1]
         ma_50 = df["SMA_50"][-1]
@@ -42,12 +47,16 @@ for i in stocklist.index:
         ma_200 = df["SMA_200"][-1]
         low_52_wk=min(df["Adj Close"][-260:])
         high_52_wk=max(df["Adj Close"][-260:])
-        
-
+        high_vol_52_wk=max(df["Volume"][-260:])
+        # current_vol = df["Volume"][-1]
+        # vol_ma_50 = df["Vol_SMA_50"][-1]
         
         try:
             ma_200_20past = df["SMA_200"][-20]
             ma_200_80past = df["SMA_200"][-80]
+
+
+
         except Exception:
             ma_200_20past = 0
             ma_200_80past = 0
@@ -91,8 +100,14 @@ for i in stocklist.index:
         #Condition 8: Relative Strength ranking (RS ranking), as repored in Investor's Business Daily, is no less than 70 (try to figure out and program it in)
 
 
-        if (cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7):
-            export_list = export_list.append({"Stock": stock, "50 Day MA": ma_50, "150 Day MA":ma_150, "200 Day MA":ma_200, "52 Week Low":low_52_wk, "52 Week High":high_52_wk},ignore_index=True)
+        #Condition 9: Current volume within 10% of max volume
+        # if (current_vol >= high_vol_52_wk*0.10):
+        #     cond_9 = True
+        # else:
+        #     cond_9 = False
+
+        if (cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7): #and cond_9
+            export_list = export_list.append({"Stock": stock, "50 Day MA": ma_50, "150 Day MA":ma_150, "200 Day MA":ma_200, "52 Week Low":low_52_wk, "52 Week High":high_52_wk},ignore_index=True) #,"52 Week Vol High":high_vol_52_wk
         else:
             print(f"{stock} doesn't meet the criteria")
         
@@ -103,8 +118,8 @@ for i in stocklist.index:
         
 print(export_list)
 
-new_file = os.path.dirname(filePath)+ "/Screen_Output.xlsx"
+# new_file = os.path.dirname(filePath)+ "/Screen_Output.xlsx"
 
-writer = ExcelWriter(new_file)
-export_list.to_excel(writer,"Sheet1")
-writer.save()
+# writer = ExcelWriter(new_file)
+# export_list.to_excel(writer,"Sheet1")
+# writer.save()
